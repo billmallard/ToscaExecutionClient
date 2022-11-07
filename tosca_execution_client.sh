c66afe15584ea7ca31a4a93d587ff944575b83bb
billmallard <86505877+billmallard@users.noreply.github.com>
2022-11-07 13:18:41 -0600

    Update tosca_execution_client.sh

--- tosca_execution_client.sh
+++ tosca_execution_client.sh
@@ -581,15 +581,19 @@ function writeResults() {
       fi
     fi
 
-    #echo -e ${executionResults} > ${resultsFilePath} 2> >(logErrorsFromStdIn)
-    printf ${executionResults} > ${resultsFilePath} 2> >(logErrorsFromStdIn)
+    echo ${executionResults} > ${resultsFilePath} 2> >(logErrorsFromStdIn)
+    #printf ${executionResults} > ${resultsFilePath} 2> >(logErrorsFromStdIn)
+    
+    #cat > ${resultsFilePath} << EOF
+    # echo ${executionResults}
+    #EOF
     
    # while read ${executionResults}
      # do
        # printf ${executionResults} > ${resultsFilePath}
      # done < "${resultsFilePath}"
     
-   log "INF" "Finished writing execution results to file \"${resultsFilePath}\""
+   log "INF" "**************Finished writing execution results to file \"${resultsFilePath}\""
   fi
 }
 
